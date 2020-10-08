be59f1c225956670cb8f5353b88a40b5a8a1e04f
Adina <adina_miki2008@yahoo.com>
2020-10-08 14:26:15 +0300

    Add api

---
+++ Api_PetApp/Controllers/AnimalsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_PetApp.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api_PetApp.Controllers
+{
+    public class AnimalsController : ControllerBase
+    {
+        List<Animal> animals = new List<Animal>();
+
+        public AnimalsController()
+        {
+            animals.Add(new Animal {
+            Id = 1,
+            Name = "an1"
+            });
+        }
+
+
+        // GET: api/<AnimalsController>
+        [Route("/animals")]
+        [HttpGet]
+        public ActionResult<List<Animal>> Get()
+        {
+            return animals;
+        }
+
+        // GET api/<AnimalsController>/5
+        [Route("/animals/{id}")]
+        [HttpGet("{id}")]
+        public ActionResult<Animal> Get(int id)
+        {
+            return animals.Where(x => x.Id == id).First();
+        }
+
+        [Route("/animals/shelter/{shelter_id}")]
+        [HttpGet("{shelter_id}")]
+        public ActionResult<List<Animal>> Get(string shelter_id)
+        {
+            return animals.Where(x => x.ShelterId == shelter_id).ToList();
+        }
+
+
+
+
+
+
+
+        //// POST api/<AnimalsController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<AnimalsController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<AnimalsController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Api_PetApp/Models/Shelter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_PetApp.Models
+{
+    public class Shelter
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Animals
+        {
+            get
+            {
+                return "https://api/animals?shelter=" + Id;
+            }
+            set { }
+        }
+
+        public string Image { get; set; }
+        public string URL { get; set; }
+        public string Email { get; set; }
+        public int Capacity { get; set; }
+        public string City { get; set; }
+    }
+}
