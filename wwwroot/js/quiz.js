124950393391e958c7432648535b721b8d9ccdcf
velicumadalina <velicumadalinaa@gmail.com>
2020-10-09 09:53:50 +0300

    Add js to quiz

--- wwwroot/js/quiz.js
+++ wwwroot/js/quiz.js
@@ -1,1 +1,10 @@
-﻿
+﻿$(document).ready(function () {
+	// When someone clicks an answer...
+	$(".answer").click(function () {
+		if (
+			!$(this).hasClass("clicked")
+		) {
+			$(this).addClass("clicked");
+		}
+	})
+})
