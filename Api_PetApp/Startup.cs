4e0f8a90404e528041091dcd774c652a09fb65a1
Adina <adina_miki2008@yahoo.com>
2020-10-20 15:24:31 +0300

    Refactor swagger Startup

--- Api_PetApp/Startup.cs
+++ Api_PetApp/Startup.cs
@@ -41,18 +41,6 @@ namespace Api_PetApp
                     Version = "v1",
                     Title = "ToDo API",
                     Description = "A simple example ASP.NET Core Web API",
-                    TermsOfService = new Uri("https://example.com/terms"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Shayne Boyer",
-                        Email = string.Empty,
-                        Url = new Uri("https://twitter.com/spboyer"),
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Use under LICX",
-                        Url = new Uri("https://example.com/license"),
-                    }
                 });
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
