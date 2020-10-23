64e52983d1aafec5b24a86b6c7412789607ac1c1
Adina <adina_miki2008@yahoo.com>
2020-10-23 04:01:16 +0300

    Refactor HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,8 +19,6 @@ namespace PetApp.Controllers
     public class HomeController : Controller
     {
         private static List<Animal> filteredAnimals = new List<Animal>();
-        private bool x;
-
         private readonly ILogger<HomeController> _logger;
         private readonly string _apiPath;
         public HomeController(ILogger<HomeController> logger)
@@ -28,6 +26,7 @@ namespace PetApp.Controllers
             _logger = logger;
             _apiPath = "https://localhost:44306/";
         }
+
 
         public async Task<IActionResult> Index()
         {
@@ -43,22 +42,26 @@ namespace PetApp.Controllers
             return View(shelterList);
         }
 
+
         [Microsoft.AspNetCore.Authorization.Authorize]
         public IActionResult Privacy()
         {
             return View();
         }
 
+
         public IActionResult Quiz()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
 
         [Route("Shelter/{id}")]
         public async Task<IActionResult> Shelter(string id)
@@ -72,11 +75,12 @@ namespace PetApp.Controllers
                     animalsList = JsonConvert.DeserializeObject<List<Animal>>(apiResponse);
                     foreach (var animal in animalsList)
                     {
-                        Dictionary<string, bool> _getsAlongWith = new Dictionary<string, bool>(){
-            { "Dogs", animal.FriendlyWithDogs },
-            { "Cats", animal.FriendlyWithCats },
-            { "Kids", animal.FriendlyWithKids }
-        };
+                        Dictionary<string, bool> _getsAlongWith = new Dictionary<string, bool>()
+                        {
+                            { "Dogs", animal.FriendlyWithDogs },
+                            { "Cats", animal.FriendlyWithCats },
+                            { "Kids", animal.FriendlyWithKids }
+                        };
 
 
                         var str = new List<string>();
@@ -93,24 +97,23 @@ namespace PetApp.Controllers
                 }
             }
             return View(animalsList);
-
         }
+
 
         [Route("/my-perfect-pets")]
         [HttpGet]
         public ActionResult<IEnumerable<Animal>> AllAnimals()
         {
             var animals = filteredAnimals;
-            Console.WriteLine(x);
             return View(filteredAnimals);
         }
+
 
         [HttpPost]
         [Consumes("application/json")]
         [Route("/my-perfect-pet")]
         public async Task<ActionResult<List<Animal>>> FilterAllAnimals([FromBody] FilteringItem filteringItem)
         {
-           
             List<Animal> animals = new List<Animal>();
             using (var httpClient = new HttpClient())
             {
@@ -120,11 +123,12 @@ namespace PetApp.Controllers
                     animals = JsonConvert.DeserializeObject<List<Animal>>(apiResponse);
                     foreach (var animal in animals)
                     {
-                        Dictionary<string, bool> _getsAlongWith = new Dictionary<string, bool>(){
-            { "Dogs", animal.FriendlyWithDogs },
-            { "Cats", animal.FriendlyWithCats },
-            { "Kids", animal.FriendlyWithKids }
-        };
+                        Dictionary<string, bool> _getsAlongWith = new Dictionary<string, bool>()
+                        {
+                            { "Dogs", animal.FriendlyWithDogs },
+                            { "Cats", animal.FriendlyWithCats },
+                            { "Kids", animal.FriendlyWithKids }
+                        };
                         var str = new List<string>();
                         foreach (KeyValuePair<string, bool> kv in _getsAlongWith)
                         {
@@ -137,7 +141,6 @@ namespace PetApp.Controllers
                     }
                 }
             }
-            Console.WriteLine(animals);
             if (filteringItem.Type[0] != "null")
             {
                 foreach (var type in filteringItem.Type)
@@ -156,14 +159,14 @@ namespace PetApp.Controllers
                 foreach (var age in filteringItem.Age)
                 {
                     animalsByAge = animals.Where(x => x.Age == age).ToList();
-                    
+
                     foreach (var animal in animalsByAge)
                     {
                         filteredByAge.Add(animal);
                     }
                 }
                 animals = filteredByAge;
-                
+
             }
             if (filteringItem.EnergyLevel[0] != "null")
             {
@@ -236,11 +239,12 @@ namespace PetApp.Controllers
                     string apiResponse = await response.Content.ReadAsStringAsync();
                     //var json = JObject.Parse(apiResponse);
                     animal = JsonConvert.DeserializeObject<Animal>(apiResponse);
-                    Dictionary<string, bool> _getsAlongWith = new Dictionary<string, bool>(){
-            { "Dogs", animal.FriendlyWithDogs },
-            { "Cats", animal.FriendlyWithCats },
-            { "Kids", animal.FriendlyWithKids }
-        };
+                    Dictionary<string, bool> _getsAlongWith = new Dictionary<string, bool>()
+                    {
+                        { "Dogs", animal.FriendlyWithDogs },
+                        { "Cats", animal.FriendlyWithCats },
+                        { "Kids", animal.FriendlyWithKids }
+                    };
 
 
                     var str = new List<string>();
@@ -252,7 +256,6 @@ namespace PetApp.Controllers
                         }
                     }
                     animal.GetsAlongWith = String.Join(",", str);
-
                 }
             }
             return View(animal);
