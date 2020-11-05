6a3260bd65b709fefb014cfe61107e1152e9fbd6
Adina <adina_miki2008@yahoo.com>
2020-11-05 15:19:34 +0200

    Add html and js for RegisterShelter

---
+++ wwwroot/js/shelter.js
@@ -0,0 +1,35 @@
+﻿let shelterImage = document.getElementById("shelter-image");
+let shelterName = document.getElementById("shelter-name");
+let shelterEmail = document.getElementById("shelter-email");
+let capacity = document.getElementById("capacity");
+
+function addShelter() {
+    let ShelterObject =
+    {
+        "Image": shelterImage.value.split("\\")[2],
+        "Name": shelterName.value,
+        "Email": shelterEmail.value,
+        "Capacity": parseInt(capacity.value)
+    }
+    return ShelterObject;
+}
+
+function sendData(endpoint, data) {
+    fetch(endpoint,
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+        .then(function (res) { console.log(res); window.location.href = "https://localhost:44335/" })
+        .catch(function (res) { console.log(res) })
+}
+
+function sendShelter() {
+    let data = addShelter();
+    console.log(data);
+    sendData("https://localhost:44306/api/Shelter", data);
+}
